065f470dac3766f6e52027501ee7b76ab31632ab
Aleksei <uporovalekseii@yandex.ru>
2022-06-15 22:11:33 +0500

    -

---
+++ front/src/components/NavBar.vue
@@ -0,0 +1,49 @@
+<template>
+        <div class="navbar"> 
+            <div class="navbar__container">
+                <div class="navbar__logo">
+                    <a href="#" class="logo__link">
+                        СНТ "Земляника"
+                    </a>
+                </div>
+                <div class="navbar__menu">
+                    <div class="menu__body">
+                        <ul class="menu__list">
+                            <li class="menu__item" v-for="menuItem in menuItems" :key="menuItem">
+                                <a href="#" class="menu__link">{{menuItem}}</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="menu__icon" id="menu__icon">
+                    <span></span>
+                </div>
+                <div class="navbar__actions">
+                    <div class="actions__login">
+                        <a href="#" class="login__link" id="link__login">Войти</a>
+                    </div>
+                    <div class="actions__icon">
+                        <img src="../assets/user.svg" alt="user" id="user__icon">
+                    </div>
+                </div>
+            </div>
+               <SubNav/>
+        </div>
+</template>
+
+<script>
+import SubNav from "@/components/SubNav.vue"
+export default {
+    components: {
+    SubNav
+  },
+        data() {
+            return {
+                menuItems:["Главная","Новости","Документы","Правление","Контакты","FAQ",]
+            }
+        },
+        
+    }
+</script>
+<style>
+</style>
---
+++ front/src/components/SubNav.vue
@@ -0,0 +1,61 @@
+<template>
+        <div class="subnav">
+            <div class="subnav__container">
+                <div class="subnav__geo">
+                    <div class="geo__icon">
+                        
+                    </div>
+                    <div class="geo__label">
+                        г.Екатеринбург,Чусовской тракт,4 км.
+                    </div>
+                </div>
+                <div class="subnav__phone">
+                    <div class="phone__icon"></div>
+                    <div class="phone__label">
+                        8 (989) 989 98 89
+                    </div>
+                </div>
+            </div>
+        </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style>
+.subnav {
+    position: fixed;
+    width: 100%;
+    height: 30px;
+    margin-top: 60px;
+    background: #d3e1e0;
+    z-index: 3;
+}
+.subnav__container {
+    width: 80%;
+    height: 100%;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-end;
+    align-items: center;
+    gap: 40px;
+    font-weight: 600;
+    color: #476160;
+}
+.subnav__geo {
+}
+.geo__icon {
+}
+.geo__label {
+}
+.subnav__phone {
+}
+.phone__icon {
+}
+.phone__label {
+}
+</style>
