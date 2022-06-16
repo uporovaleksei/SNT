fcee614d6e840991d3e1d17378c9aa9eab995174
Aleksei <uporovalekseii@yandex.ru>
2022-06-16 17:05:56 +0500

    -

---
+++ front/src/views/NewsPage.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <NavBar/>
+    <SubNav/>
+    <News/>
+    <Footer/>
+  </div>
+</template>
+
+<script>
+  import News from "@/components/NewsItem.vue"
+  import Footer from "@/components/Footer.vue"
+  export default {
+    components: {
+      News,
+      Footer,
+    },
+    data() {
+      return {
+        
+      }
+    },
+  }
+</script>
+
+
+<style>
+</style>
