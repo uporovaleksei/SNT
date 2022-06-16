e587ca82e136dae4b2ea3c07bfda54b41647deb1
Nikita Abaturov <35378637+KeeVeeG@users.noreply.github.com>
2022-06-16 17:22:58 +0500

    add layots

---
+++ front/src/layouts/Main.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <NavBar/>
+    <slot/>
+    
+  </div>
+</template>
+
+<script>
+import NavBar from '@/components/NavBar.vue'
+export default {
+  components: {
+    NavBar
+  }
+}
+</script>
+
+<style>
+
+</style>
